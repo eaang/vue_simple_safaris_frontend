9dee538cf9b53272e4edee497d80239ba8680613
Evan Ang <alethiologist@gmail.com>
2020-08-29 22:08:48 +0800

    new frontend project

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Noto Sans KR',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+    }
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
