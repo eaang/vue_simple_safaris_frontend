813907245af69e7f4eb7460c27862352552ed881
Evan Ang <alethiologist@gmail.com>
2020-08-30 22:27:49 +0800

    added containers

--- components/CardsIntro.vue
+++ components/CardsIntro.vue
@@ -1,16 +1,16 @@
 <template>
   <!-- Card Container -->
-  <div class="mx-8 grid grid-flow-row xl:grid-flow-col">
+  <div class="container mx-auto grid grid-flow-row xl:grid-flow-col">
     <!-- Card One -->
     <div class="">
-      <div class="card-image">
+      <div class="intro-card-image">
         <img class="" src="@/assets/icon-01.svg" />
       </div>
       <div class="">
-        <div class="card-title title-dark">
+        <div class="intro-card-title title-dark">
           사파리 여행 전문가와 함께하는 플래닝
         </div>
-        <div class="card-text content-light">
+        <div class="intro-card-text content-light">
           여행 상담부터 여행 다녀오실 때까지<br />
           전문 사파리 여행 컨설턴트가 고객님을 도와드립니다.<br />
           복잡하고 어렵게만 느껴졌던 사파리 여행이 간편해집니다.
@@ -19,14 +19,14 @@
     </div>
     <!-- Card Two -->
     <div class="">
-      <div class="card-image">
+      <div class="intro-card-image">
         <img class="" src="@/assets/icon-02.svg" />
       </div>
       <div class="">
-        <div class="card-title title-dark">
+        <div class="intro-card-title title-dark">
           100% 맞춤형 사파리 여행
         </div>
-        <div class="card-text content-light">
+        <div class="intro-card-text content-light">
           심플 사파리는 누구나를 위해 만들어진<br />
           패키지 여행은 판매하지 않습니다.<br />
           고객의 취향을 반영해  맞춤 여행을 기획해드립니다.
@@ -35,14 +35,14 @@
     </div>
     <!-- Card Three -->
     <div class="">
-      <div class="card-image">
+      <div class="intro-card-image">
         <img class="" src="@/assets/icon-03.svg" />
       </div>
       <div class="">
-        <div class="card-title title-dark">
+        <div class="intro-card-title title-dark">
           Adventure & Relaxation
         </div>
-        <div class="card-text content-light">
+        <div class="intro-card-text content-light">
           사파리 여행 동안 모험과 휴양을 동시에 즐길 수 있도록<br />
           심플 사파리의 자존심을 걸고<br />
           가장 훌륭한 숙소에서 지내실 수 있도록 하겠습니다.
@@ -54,18 +54,18 @@
 </template>
 
 <style>
-.card-image {
+.intro-card-image {
   @apply h-48 flex justify-center items-end;
 }
-.card-title {
+.intro-card-title {
   @apply py-4 font-bold text-center text-3xl;
 }
-.card-text {
+.intro-card-text {
   line-height: 1.5;
-  @apply italic text-center px-6 text-xl;
+  @apply italic text-center px-6 text-base;
 }
 @media (min-width: 1280px) { 
-  .card-text {
+  .intro-card-text {
     @apply h-48;
   }
 }
---
+++ components/CardsStyle.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Card Container -->
+  <div class="container mx-auto grid grid-cols-4 gap-8">
+
+    <!-- Selected Card -->
+    <div class="">
+      <div>
+        <img class="" src="@/assets/images/trip1.png" />
+      </div>
+
+      <div>
+      </div>
+    </div>
+
+    <!-- Normal Card -->
+    <div class="">
+      <div>
+        <img class="" src="@/assets/images/trip2.png" />
+      </div>
+
+      <div>
+      </div>
+    </div>
+
+    <!-- Normal Card -->
+    <div class="">
+      <div>
+        <img class="" src="@/assets/images/trip3.png" />
+      </div>
+
+      <div>
+      </div>
+    </div>
+
+    <!-- Normal Card -->
+    <div class="">
+      <div>
+        <img class="" src="@/assets/images/trip4.png" />
+      </div>
+
+      <div>
+      </div>
+    </div>
+  <!-- End of Card Container -->
+  </div>
+</template>
