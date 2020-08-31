ca864ff7215c549143e5a49f3d690bf17261b89e
Evan Ang <alethiologist@gmail.com>
2020-08-31 19:33:34 +0800

    nevermind, contact us page is back to 3/4 height

--- pages/success.vue
+++ pages/success.vue
@@ -4,24 +4,32 @@
 
     <!-- Header Section -->
     <div class="">
-      <div class="bg-local contact-pic h-screen/2 relative">
+      <div class="bg-local contact-pic h-screen/75 relative">
         <div class="h-screen/2 text-white text-5xl text-center flex justify-center items-center pt-8">
           Contact us
         </div>
-        <div class="container mx-auto bg-gray-300 absolute inset-x-0 bottom-0 pt-16 px-8 space-y-4">
+
+        <!-- Success Container Top Half -->
+        <div class="container mx-auto success-box absolute inset-x-0 bottom-0 pt-16 px-8">
           <div class="text-3xl text-center dark-text">
             사파리 여행 문의가 성공적으로  접수되었습니다!
           </div>
           <!-- Line Break -->
-          <div class="flex justify-center mt-16">
+          <div class="flex justify-center mt-8">
             <div class="divider-line"></div>
           </div>
         </div>
       </div>
-      <div class="container mx-auto bg-gray-300 inset-x-0 bottom-0 pb-16 px-8">
-        <div class="text-2xl text-center light-text">
-            문의주신 내용을 바탕으로 24시간 이내에 사파리여행 컨설턴트가 연락 드릴 예정입니다.<br />
-            인생에서 가장 기억에 남을 만한 사라피여행을 만들어 드리겠습니다.
+      <!-- Success Container Bottom Half -->
+      <div class="container mx-auto success-box inset-x-0 bottom-0 pb-16 pt-8 px-8">
+        <div class="text-2xl text-center success-message">
+            <div>
+              문의주신 내용을 바탕으로 <span class="dark-text">24시간 이내</span>에 
+              사파리여행 컨설턴트가 연락 드릴 예정입니다.
+            </div>
+            <div class="italic">
+              인생에서 가장 기억에 남을 만한 사라피여행을 만들어 드리겠습니다.
+            </div>
         </div>
       </div>
       
@@ -32,6 +40,12 @@
 </template>
 
 <style>
+  .success-message {
+    color: #535043;
+  }
+  .success-box {
+    background-color: #f0f0ec;
+  }
   .contact-pic {
     background: url('../assets/images/hero.png') no-repeat center center / cover;
   }
