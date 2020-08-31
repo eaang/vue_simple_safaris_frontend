ac97013e99bfe4f4113a6f17feed83e1bf019522
Evan Ang <alethiologist@gmail.com>
2020-08-31 13:31:13 +0800

    made copyright smaller

--- components/CopyrightInfo.vue
+++ components/CopyrightInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center text-lg p-8 flex flex-wrap justify-center">
+  <div class="text-center p-6 flex flex-wrap justify-center">
     <div class="mr-1">2020 © Copyright Simple Safaris.</div>
     <div>All Rights Reserved.</div>
   </div>
