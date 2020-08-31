c01603003bdffd416e898a5a9d041edab30b0ee7
Evan Ang <alethiologist@gmail.com>
2020-08-31 13:30:31 +0800

    fixed copyright message

--- components/AboutMap.vue
+++ components/AboutMap.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto my-24">
-    <div class="map-container h-screen/75">
-      <img src="@/assets/pin.svg">
+    <div class="relative map-container h-screen/2 lg:h-screen/75">
+      <img class="map-pin absolute" src="@/assets/pin.svg">
     </div>
   </div>
 </template>
@@ -9,5 +9,9 @@
 <style scoped>
   .map-container {
     background-color: #ececec;
+  }
+  .map-pin {
+    right: 200px;
+    top: 100px;
   }
 </style>
--- components/CopyrightInfo.vue
+++ components/CopyrightInfo.vue
@@ -1,3 +1,6 @@
 <template>
-  <div class="text-center text-lg p-8">2020 © Copyright Simple Safaris. All Rights Reserved</div>
+  <div class="text-center text-lg p-8 flex flex-wrap justify-center">
+    <div class="mr-1">2020 © Copyright Simple Safaris.</div>
+    <div>All Rights Reserved.</div>
+  </div>
 </template>
