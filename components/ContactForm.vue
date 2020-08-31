ddaabaad4bd7aa7d524b79e0106556c7fb3ef43d
Evan Ang <alethiologist@gmail.com>
2020-08-31 15:43:29 +0800

    contact form background made

---
+++ components/ContactForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="contact-form container h-screen">
+
+  </div>
+</template>
+
+<style>
+  .contact-form {
+    border-top: 3px solid #9c9780;
+    border-bottom: 3px solid #9c9780;
+    background-color: #F0F0ED;
+  }
+</style>
